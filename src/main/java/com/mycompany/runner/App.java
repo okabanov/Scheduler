07c937971010d89e9ce1c9b3d4fbb90d0f4de01b
Oleg <kabanov.o.a@gmail.com>
2013-03-04 23:32:04 +0400

    refactor algorithm

--- src/main/java/com/mycompany/runner/App.java
+++ src/main/java/com/mycompany/runner/App.java
@@ -3,51 +3,31 @@ package com.mycompany.runner;
 import java.io.IOException;
 import java.util.Date;
 
-public class App {
+// for (var i = 0; i < a.length; i++) {var tmp = a[i].split(':'); a[i] = tmp[0] * 60 + tmp[1] * 1}
 
-    private static int[] intervals = new int[5];
+public class App {
+    private static int delayInSeconds = 1;
+    private static int timePeriod = 30 * 60;
+    private static float rate = 0.22f; // 200 users
+    private static int[] intervals = new int[] {
+        3, 6, 1, 2, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 16, 1, 7, 20, 32, 13, 
+        17, 10, 26, 41, 16, 24, 16, 27, 17, 47, 38, 16, 29, 44, 39, 31, 28, 71, 
+        56, 53, 44, 28, 41, 26, 19, 4
+    }; // summ = 913
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        // s = s.replace(/.*:(\d\d:\d\d:\d\d).*/, "$1")
-        // a = s.split(':')
-        // t = a[0] * 3600 + a[1] * 60 + a[2]
-        createTestData();
         while (true) {
-            int intervalPosition = searchInterval(getCurrentTime());
-            if (intervalPosition >= 0) {
-                for (int i = intervalPosition; i < intervals.length; i++) {
-                    Runtime.getRuntime().exec("ls");
-                    int timeout = intervals[i] - getCurrentTime();
-                    if (timeout > 0) {
-                        Thread.sleep(timeout * 1000);
-                    }
-                }
+            if(Math.random() * timePeriod / delayInSeconds < intervals[getCurrentTime()] * rate) {
+                Runtime.getRuntime().exec("runner.bat");
             }
-            Thread.sleep(1000); // Полночь. И такая фигня пока не придёт первый пользователь.
+            Thread.sleep(delayInSeconds * 1000);
         }
-    }
-
-    private static int searchInterval(int time) {
-        for (int i = 0; i < intervals.length; i++) {
-            if (intervals[i] > time) {
-                return i;
-            }
-        }
-        return -1; // На сегодня больше нет
     }
 
     private static int getCurrentTime() {
         Date date = new Date();
-        return (date.getHours() + 1) * 3600
-                + date.getMinutes() * 60
-                + date.getSeconds();
+        return (int) Math.floor((date.getHours() * 3600 + date.getMinutes() * 60) / timePeriod);
     }
 
-    private static void createTestData() {
-        int time = getCurrentTime();
-        for (int i = 0; i < intervals.length; i++) {
-            time += Math.random() * 10;
-            intervals[i] = time;
-        }
-    }
+    
 }
